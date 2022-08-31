df3c3615e1ad47d2dbae209c4b13d1c2f3171067
Daniel Faustino <daniel@dcx.ufpb.br>
2022-08-31 08:22:50 -0300

    Teste

--- src/main/java/br/ufpb/dcx/dsc/todolist/TodolistApplication.java
+++ src/main/java/br/ufpb/dcx/dsc/todolist/TodolistApplication.java
@@ -8,6 +8,7 @@ public class TodolistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodolistApplication.class, args);
+        System.out.println("Ola mundo");
     }
 
 }
