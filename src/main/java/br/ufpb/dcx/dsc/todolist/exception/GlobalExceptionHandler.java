315e19d90bc4df6d6915ac5ee18685da4cb7aad0
Daniel Souza <daniel@dcx.ufpb.br>
2025-09-06 16:21:57 -0300

    Update libs

--- src/main/java/br/ufpb/dcx/dsc/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/br/ufpb/dcx/dsc/todolist/exception/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestControllerAdvice
+@RestControllerAdvice()
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({
